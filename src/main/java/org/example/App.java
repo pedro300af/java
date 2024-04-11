482d1dc4e9130d77cd767fe8205e1b17f611b943
Pedro Arriaga <72060879+pedro300af@users.noreply.github.com>
2024-04-10 22:05:14 -0600

    UPDATE: add class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,15 +5,6 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<Persona> lista = new ArrayList<Persona>();
-        lista.add(new Persona(1, "pedro1", 27));
-        lista.add(new Persona(2, "pedro2", 27));
-        lista.add(new Persona(3, "pedro3", 27));
-        lista.add(new Persona(4, "pedro4", 27));
-        lista.add(new Persona(5, "pedro5", 27));
 
-        for (Persona persona : lista) {
-            System.out.println(persona.getNombre());
-        }
     }
 }
---
+++ src/main/java/org/example/logica/Alumno.java
@@ -0,0 +1,7 @@
+package org.example.logica;
+
+public class Alumno {
+    private int id;
+    private String apellido;
+
+}
