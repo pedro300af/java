6ace2710d09b4fb0d256d19c65550a466458068a
Pedro Arriaga <72060879+pedro300af@users.noreply.github.com>
2024-04-10 22:12:06 -0600

    UPDATE: methods

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.example.logica.Alumno;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-
+        Alumno alumno1 = new Alumno();
+        alumno1.saberAprobado(5);
     }
 }
--- src/main/java/org/example/logica/Alumno.java
+++ src/main/java/org/example/logica/Alumno.java
@@ -4,4 +4,15 @@ public class Alumno {
     private int id;
     private String apellido;
 
+    public void mostrarNombre(){
+        System.out.println("Hola mi nombre es: ");
+    }
+
+    public void saberAprobado(double calificacion){
+        if (calificacion >= 6){
+            System.out.println("aprobe la materia");
+        }else {
+            System.out.println("uyy no aprobe");
+        }
+    }
 }
