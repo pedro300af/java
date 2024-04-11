a47c21a65cf4e6984fb74504ede95b53d1fd83a1
Pedro Arriaga <72060879+pedro300af@users.noreply.github.com>
2024-04-10 22:22:37 -0600

    UPDATE: add setter, getter and constructor

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        Alumno alumno1 = new Alumno();
+        Alumno alumno1 = new Alumno(1, "Arriaga");
         alumno1.saberAprobado(5);
+        alumno1.mostrarNombre();
     }
 }
--- src/main/java/org/example/logica/Alumno.java
+++ src/main/java/org/example/logica/Alumno.java
@@ -4,8 +4,33 @@ public class Alumno {
     private int id;
     private String apellido;
 
+    public Alumno() {
+
+    }
+
+    public Alumno(int id, String apellido) {
+        this.id = id;
+        this.apellido = apellido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
     public void mostrarNombre(){
-        System.out.println("Hola mi nombre es: ");
+        System.out.println("Hola mi nombre es: " + apellido);
     }
 
     public void saberAprobado(double calificacion){
