a2dbb43077ca18c4f7ce39faa457987912254055
Pedro Arriaga <72060879+pedro300af@users.noreply.github.com>
2024-04-13 19:39:45 -0600

    UPDATE

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        Alumno alumno1 = new Alumno(1, "pedro", "aee");
 
+        Alumno alumno2 = new Alumno();
+
+//        alumno1.setNombre("pedro");
+
+        System.out.println(alumno1.getNombre());
     }
 }
